7bd592cc4abc0981e957cc57246d28d3978b3de6
AngeloAugustoCSW <angelo.weimar@hotmail.com>
2023-02-08 22:07:47 -0300

    init

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text> Welcome to home page</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignContent: "center",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>My App</Text>
+    </View>
+  );
+}
