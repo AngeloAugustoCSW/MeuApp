5873f0178727ba73aec045788e09f2783b9f133f
Angelo Augusto CSW <angelo.weimar@hotmail.com>
2023-02-22 21:51:37 -0300

    Added style sheet and screens

---
+++ src/screens/AboutScreen.jsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import { styles } from "../utils/styles";
+
+export default function AboutScreen({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text>Olá eu sou a About Screen</Text>
+      <Button
+        mode="outlined"
+        onPress={() => navigation.navigate("Contact")} 
+      >Ir para Contato</Button>
+    </View>
+  );
+}
---
+++ src/screens/ContactScreen.jsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "../utils/styles";
+
+export default function ContactScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá eu sou a Contact Screen</Text>
+    </View>
+  );
+}
